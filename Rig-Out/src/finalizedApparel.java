0746ad2e08b3b6ce7c38da9dcea4877cbfec10bb
Addis <Addis@DESKTOP-89TCULG>
2020-10-30 20:38:29 -0700

    First Commit

---
+++ Rig-Out/src/finalizedApparel.java
@@ -0,0 +1,14 @@
+
+public interface finalizedApparel {
+	//default method for getting the finalized outfit
+	default void getOutfit() {
+		
+	}
+	//default method for getting the default outfit
+	default void getDefaultOutfit() {
+		
+	}
+	 static void message() {
+		System.out.println("Processing Apparel Selection");
+	}
+}
